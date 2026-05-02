18bd3822b225c28a351f2ba19637287db422a6f9
Ryan Hellyer <ryanhellyer@gmail.com>
2026-05-02 19:43:46 +0000

    feat: bump version to 3.0

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,4 +3,4 @@ package version
 // Version is the release version. Override at link time, for example:
 //
 //	go build -ldflags "-X gitmeh/internal/version.Version=1.0.0"
-var Version = "0.1"
+var Version = "3.0"
